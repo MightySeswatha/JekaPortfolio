bd727febdfe285ca099a1e36b92cc6b4377db781
MightySeswatha <teszerorobot@gmail.com>
2022-09-17 20:08:08 +0300

    new11

---
+++ scripts/script1.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+console.log("Hola!.This page is loaded!")
+const m_btn = document.getElementById("menu_btn");
+const m = document.getElementById("menu");
+
+m_btn.onclick = function(){
+m.classList.toggle("menu-list");
+}
+
+};
