6f2e69ef50bb1213071eb1d04a41cc4f9f62870c
MightySeswatha <teszerorobot@gmail.com>
2022-09-18 16:50:57 +0300

    new13

--- scripts/script1.js
+++ scripts/script1.js
@@ -2,10 +2,17 @@ window.onload = function(){
 console.log("Hola!.This page is loaded!")
 const m_btn = document.getElementById("menu_btn");
 const m = document.getElementById("menu");
+const m_btn1 = document.getElementById("menu_btn1");
+const m1 = document.getElementById("menu1");
 
 m_btn.onclick = function(){
 m.classList.toggle("menu-list");
 m.classList.toggle("off");
+}
+
+m_btn1.onclick = function(){
+m1.classList.toggle("menu-list");
+m1.classList.toggle("off");
 }
 
 };
