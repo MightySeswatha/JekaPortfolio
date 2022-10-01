1bf0a40a12a8ec6d446d5163aed8ab0239d4402a
MightySeswatha <teszerorobot@gmail.com>
2022-10-01 20:16:03 +0300

    new25

--- scripts/script1.js
+++ scripts/script1.js
@@ -4,10 +4,6 @@ const m_btn = document.getElementById("menu_btn");
 const m = document.getElementById("menu");
 const m_btn1 = document.getElementById("menu_btn1");
 const m1 = document.getElementById("menu1");
-const s_btn = document.getElementById("s_btn");
-const shadow_inner = document.getElementById("shadow_inner");
-const con_form = document.getElementById("con_form");
-const shadow = document.getElementById("shadow");
 
 m_btn.onclick = function(){
 m.classList.toggle("menu-list");
@@ -17,12 +13,6 @@ m.classList.toggle("off");
 m_btn1.onclick = function(){
 m1.classList.toggle("menu-list");
 m1.classList.toggle("off");
-}
-
-s_btn.onclick = function(){
-shadow_inner.classList.toggle("off");
-con_form.classList.toggle("off");
-shadow.classList.toggle("i_max");
 }
 
 };
